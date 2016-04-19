5aeca70e47bf9546379638b89f527a3036e49acb
Miztah87 <zalantassaly@gmail.com>
2016-04-19 09:04:25 +0200

    Empty behavior pattern created.

---
+++ src/ForwardBehave.java
@@ -0,0 +1,31 @@
+import lejos.robotics.navigation.DifferentialPilot;
+import lejos.robotics.subsumption.Behavior;
+
+public class ForwardBehave implements Behavior {
+	boolean supress = false;
+	
+	
+	DifferentialPilot pilot;
+	public ForwardBehave(DifferentialPilot pilot){
+		this.pilot = pilot;
+	}
+	
+	@Override
+	public boolean takeControl() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void action() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void suppress() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,44 @@
 
-public class Main {
+import lejos.nxt.Button;
+import lejos.nxt.Motor;
+import lejos.robotics.navigation.DifferentialPilot;
+import lejos.robotics.subsumption.Arbitrator;
+import lejos.robotics.subsumption.Behavior;
 
+public class Main {
+	private static DifferentialPilot pilot = new DifferentialPilot(
+		    DifferentialPilot.WHEEL_SIZE_NXT2, 14.0, Motor.B, Motor.C);
+	   
 	public static void main(String[] args) {
-		
+		waitForKeyPressWithDelay(500);
+    	pilot.setTravelSpeed(500);
+    	System.out.println("Program Started");
+    	Behavior forward = new ForwardBehave(pilot);
+    	Behavior stop = new ExitBehave(pilot);
+    	Behavior detect = new ObjectDetectorBehave(pilot);
+    	Behavior[] behave = {forward, detect, stop};
+    	
+    	
+        Arbitrator arb = new Arbitrator(behave);
+    	arb.start();
 	}
+	
+	public static void waitForKeyPressWithDelay(int delayTime)
+    {
+	System.out.println("Press any key to continue...");
+	Button.waitForAnyPress();
+	delay(delayTime);
+    }
+
+    public static void delay(int delayTime)
+
+    {
+	try
+	{
+	    Thread.sleep(delayTime);
+	} catch (InterruptedException e)
+	{
+	    // just wake up
+	}
+    }
 }
